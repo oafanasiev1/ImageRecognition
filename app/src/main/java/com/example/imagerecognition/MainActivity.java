2422865955b31424e71349b6f2eabaff5402360f
Alex Afanasiev <oafanasiev@css.edu>
2019-04-27 17:10:36 -0500

    UI implemented; The app receives working API

---
+++ app/src/main/java/com/example/imagerecognition/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.imagerecognition;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "Tag";
+    private TextView txtView;
+    private Button snapBtn, detectBtn;
+    private ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    Bitmap imageBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        txtView = findViewById(R.id.textView);
+        snapBtn = findViewById(R.id.btnSnap);
+        detectBtn = findViewById(R.id.btnDetect);
+        imageView = findViewById(R.id.imageView);
+//        snapBtn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                dispatchTakePictureIntent();
+//            }
+//        });
+
+//        detectBtn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                detectImage();
+//            }
+//        });
+
+    }
+
+
+    public void takeImage(View view){
+        dispatchTakePictureIntent();
+    }
+
+
+    public void recognizeImage(View view){
+        detectImage();
+    }
+
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void detectImage(){
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance().getOnDeviceImageLabeler();
+
+        labeler.processImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+
+            @Override
+            public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                float maxConfidence;
+                for (FirebaseVisionImageLabel label: labels){
+
+                    Log.d(TAG, label.getText());
+                    Log.d(TAG, label.getConfidence() + "");
+
+                    txtView.setTextSize(20);
+
+                    txtView.setText(label.getText());
+
+                //   Toast.makeText(this, label.getText(), Toast.LENGTH_SHORT).show();
+
+                    maxConfidence = label.getConfidence();
+
+
+                }
+
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        txtView.setText("Cannot recognize");
+                        Log.d("error:", "Cannot recognize");
+                    }
+                });
+
+    }
+
+
+
+}
